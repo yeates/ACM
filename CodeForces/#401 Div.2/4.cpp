23ccafee4b1a0f72df1a4ad514bd3def82478d44
yeates <yeates98@hotmail.com>
2017-02-25 10:50:14 +0800

    #401 AK

--- CodeForces/#401 Div.2/4.cpp
+++ CodeForces/#401 Div.2/4.cpp
@@ -1,3 +1,8 @@
+/*
+一次ac。
+题意分析，模拟，剪枝优化
+优化cin:1310ms
+*/
 #include <bits/stdc++.h>
 #define FOR(i, a, b) for (int i = (a); i <= (b); i++)
 #define FORD(i, a, b) for (int i = (a); i >= (b); i--)
@@ -6,43 +11,52 @@
 #define pi acos(-1)
 #define Max(a, b) a > b ? a: b
 #define Min(a, b) a < b ? a: b
-#define MAX 100005
+#define MAX 500005
 #define LL long long
 #define INF 0x3f3f3f3f
 using namespace std;
 
-
+vector<int>a[MAX];
 
 int main(){
     ios::sync_with_stdio(0);
-    int n, l, r;
-    while(cin >> n >> l >> r){
-        int a[MAX], b[MAX], c[MAX], p[MAX], z[MAX];
-        REP(i, n)   cin >> a[i];
+    int n;
+    while(cin >> n){
+        string tp;
         REP(i, n){
-            int tp;
             cin >> tp;
-            p[i] = tp;
-            z[tp-1] = i;
+            FOR(j, 1, tp.size()-1){
+                a[i].pb(tp.at(j)-'a');
+            }
+            a[i].pb(-1);
+        }
+        FORD(i, n-2, 0){
+            bool flag = true;
+            REP(j, a[i].size()-1){
+                if(a[i+1][j] < a[i][j] && a[i+1][j] != -1){
+                    a[i][j] = -1;
+                    break;
+                }
+                else if(a[i+1][j] > a[i][j])    break;
+                else if(a[i+1][j] == -1){
+                    if(flag){
+                        a[i][j] = -1;
+                        break;
+                    }
+                    else break;
+                }
+            }
         }
         REP(i, n){
-            int x = z[i];
-            if(i == 0){
-                b[x] = l;
-                c[x] = b[x] - a[x];
+            cout << "#";
+            int j = 0;
+            while(a[i][j] != -1){
+                cout << char(a[i][j] + 'a');
+                j++;
             }
-            else{
-                b[x] = c[z[i-1]] + a[x] + 1;
-                if(b[x] < l)    b[x] = l;
-                c[x] = b[x] - a[x];
-            }
+            cout << endl;
         }
-        if(b[n-1] > r)  cout << "-1" << endl;
-        else{
-            REP(i, n){
-                i == n-1 ?cout << b[i] << endl: cout << b[i] << " ";
-            }
-        }
-     }
+        REP(i, n)   a[i].clear();
+    }
     return 0;
 }
