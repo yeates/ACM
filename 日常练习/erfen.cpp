0f1af124a40447f8a33a95f1fe7eabef11ecbefc
yeates <yeates98@hotmail.com>
2017-03-01 22:45:02 +0800

    lianxi

--- 日常练习/erfen.cpp
+++ 日常练习/erfen.cpp
@@ -9,12 +9,13 @@ using namespace std;
 int erfen(int a[], int n, int item){
     int l = 0, r = n-1;
     while(l <= r){
-        int mid = (l + r) / 2;
+        //int mid = (l + r) / 2;
+        int mid = (r - l) / 2 + l;
         if(a[mid] == item)  return mid;
         if(a[mid] < item)   l = mid + 1;
         else r = mid - 1;
     }
-    return l;
+    return -2;
 }
 
 int main(){
